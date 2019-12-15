d9d50f97ae386a321c2fc24be805fcee0201ca68
Daniel Svindseth <daniel24.gitar@gmail.com>
2019-12-15 21:57:36 +0100

    Wow themes

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,27 @@
-// Switch Layout
-
-// *** TO BE CUSTOMISED ***
-
-var style_cookie_name = "style" ;
-var style_cookie_duration = 30 ;
-var style_domain = "thesitewizard.com" ;
-
-// *** END OF CUSTOMISABLE SECTION ***
-// You do not need to customise anything below this line
-
-function switch_style ( css_title )
-{
-// You may use this script on your site free of charge provided
-// you do not remove this notice or the URL below. Script from
-// https://www.thesitewizard.com/javascripts/change-style-sheets.shtml
-  var i, link_tag ;
-  for (i = 0, link_tag = document.getElementsByTagName("link") ;
-    i < link_tag.length ; i++ ) {
-    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
-      link_tag[i].title) {
-      link_tag[i].disabled = true ;
-      if (link_tag[i].title == css_title) {
-        link_tag[i].disabled = false ;
-      }
-    }
-    set_cookie( style_cookie_name, css_title,
-      style_cookie_duration, style_domain );
+//much java such script
+function thm(){
+  if (localStorage.theme==2) {
+    localStorage.setItem("theme", "1");
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm1.css');
+  }
+  else {
+    localStorage.setItem("theme", "2");
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm2.css');
   }
 }
-function set_style_from_cookie()
-{
-  var css_title = get_cookie( style_cookie_name );
-  if (css_title.length) {
-    switch_style( css_title );
+
+function loadTheme(){
+  if (localStorage.theme==1) {
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm1.css');
+  }
+  else {
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm2.css');
   }
 }
-function set_cookie ( cookie_name, cookie_value,
-    lifespan_in_days, valid_domain )
-{
-    // https://www.thesitewizard.com/javascripts/cookies.shtml
-    var domain_string = valid_domain ?
-                       ("; domain=" + valid_domain) : '' ;
-    document.cookie = cookie_name +
-                       "=" + encodeURIComponent( cookie_value ) +
-                       "; max-age=" + 60 * 60 *
-                       24 * lifespan_in_days +
-                       "; path=/" + domain_string ;
-}
-function get_cookie ( cookie_name )
-{
-    // https://www.thesitewizard.com/javascripts/cookies.shtml
-	var cookie_string = document.cookie ;
-	if (cookie_string.length != 0) {
-		var cookie_array = cookie_string.split( '; ' );
-		for (i = 0 ; i < cookie_array.length ; i++) {
-			cookie_value = cookie_array[i].match ( cookie_name + '=(.*)' );
-			if (cookie_value != null) {
-				return decodeURIComponent ( cookie_value[1] ) ;
-			}
-		}
-	}
-	return '' ;
+
+function firstVisit(){
+  if (localStorage.prevVisit!=1) {
+    alert("This site uses cookies! By using this site you agree to saving a few bits in localStorage");
+    localStorage.setItem("prevVisit", "1")
+  }
 }
