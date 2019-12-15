4aece1b3d243c7a755de6c3c6bc0f62ccb6c2b66
Daniel Svindseth <daniel24.gitar@gmail.com>
2019-12-15 22:35:14 +0100

    much bugs wow

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 //much java such script
 function thm(){
-  if (localStorage.theme==2) {
+  if (localStorage.theme==1){
+    localStorage.setItem("theme", "2");
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm2.css');
+  }
+  if (localStorage.theme==2){
+    localStorage.setItem("theme", "3");
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm3.css');
+  }
+  if (localStorage.theme==3){
     localStorage.setItem("theme", "1");
     document.getElementById('wow-theme').setAttribute('href', 'css/thm1.css');
   }
   else {
-    localStorage.setItem("theme", "2");
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm2.css');
+    break;
   }
 }
 
@@ -14,8 +21,11 @@ function loadTheme(){
   if (localStorage.theme==1) {
     document.getElementById('wow-theme').setAttribute('href', 'css/thm1.css');
   }
-  else {
+  if (localStorage.theme==2) {
     document.getElementById('wow-theme').setAttribute('href', 'css/thm2.css');
+  }
+  else {
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm3.css');
   }
 }
 
