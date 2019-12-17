1d3ad746c2e01ba2151535371b3a7383a1761e59
Daniel Svindseth <daniel24.gitar@gmail.com>
2019-12-17 19:38:37 +0100

    backup commit

--- flashcards/set1.js
+++ flashcards/set1.js
@@ -1,86 +1,84 @@
-function start(){
+
   //Row 1
-  document.getElementById("r1c1q").innerHTML = "1000";
+  document.getElementById("r1c1q1").innerHTML = "1";
   document.getElementById("r1c1a1").innerHTML = "";
   document.getElementById("r1c1a2").innerHTML = "";
 
-  document.getElementById("r1c2q").innerHTML = "2";
+  document.getElementById("r1c2q1").innerHTML = "2";
   document.getElementById("r1c2a1").innerHTML = "";
   document.getElementById("r1c2a2").innerHTML = "";
 
-  document.getElementById("r1c3q").innerHTML = "3";
+  document.getElementById("r1c3q1").innerHTML = "3";
   document.getElementById("r1c3a1").innerHTML = "";
   document.getElementById("r1c3a2").innerHTML = "";
 
-  document.getElementById("r1c4q").innerHTML = "4";
+  document.getElementById("r1c4q1").innerHTML = "4";
   document.getElementById("r1c4a1").innerHTML = "";
   document.getElementById("r1c4a2").innerHTML = "";
   //Row 2
-  document.getElementById("r2c1q").innerHTML = "5";
+  document.getElementById("r2c1q1").innerHTML = "5";
   document.getElementById("r2c1a1").innerHTML = "";
   document.getElementById("r2c1a2").innerHTML = "";
 
-  document.getElementById("r2c2q").innerHTML = "6";
+  document.getElementById("r2c2q1").innerHTML = "6";
   document.getElementById("r2c2a1").innerHTML = "";
   document.getElementById("r2c2a2").innerHTML = "";
 
-  document.getElementById("r2c3q").innerHTML = "7";
+  document.getElementById("r2c3q1").innerHTML = "7";
   document.getElementById("r2c3a1").innerHTML = "";
   document.getElementById("r2c3a2").innerHTML = "";
 
-  document.getElementById("r2c4q").innerHTML = "8";
+  document.getElementById("r2c4q1").innerHTML = "8";
   document.getElementById("r2c4a1").innerHTML = "";
   document.getElementById("r2c4a2").innerHTML = "";
 
   //Row 3
-  document.getElementById("r3c1q").innerHTML = "9";
+  document.getElementById("r3c1q1").innerHTML = "9";
   document.getElementById("r3c1a1").innerHTML = "";
   document.getElementById("r3c1a2").innerHTML = "";
 
-  document.getElementById("r3c2q").innerHTML = "10";
+  document.getElementById("r3c2q1").innerHTML = "10";
   document.getElementById("r3c2a1").innerHTML = "";
   document.getElementById("r3c2a2").innerHTML = "";
 
-  document.getElementById("r3c3q").innerHTML = "11";
+  document.getElementById("r3c3q1").innerHTML = "11";
   document.getElementById("r3c3a1").innerHTML = "";
   document.getElementById("r3c3a2").innerHTML = "";
 
-  document.getElementById("r3c4q").innerHTML = "12";
+  document.getElementById("r3c4q1").innerHTML = "12";
   document.getElementById("r3c4a1").innerHTML = "";
   document.getElementById("r3c4a2").innerHTML = "";
 
   //Row 4
-  document.getElementById("r4c1q").innerHTML = "13";
+  document.getElementById("r4c1q1").innerHTML = "13";
   document.getElementById("r4c1a1").innerHTML = "";
   document.getElementById("r4c1a2").innerHTML = "";
 
-  document.getElementById("r4c2q").innerHTML = "14";
+  document.getElementById("r4c2q1").innerHTML = "14";
   document.getElementById("r4c2a1").innerHTML = "";
   document.getElementById("r4c2a2").innerHTML = "";
 
-  document.getElementById("r4c3q").innerHTML = "15";
+  document.getElementById("r4c3q1").innerHTML = "15";
   document.getElementById("r4c3a1").innerHTML = "";
   document.getElementById("r4c3a2").innerHTML = "";
 
-  document.getElementById("r4c4q").innerHTML = "16";
+  document.getElementById("r4c4q1").innerHTML = "16";
   document.getElementById("r4c4a1").innerHTML = "";
   document.getElementById("r4c4a2").innerHTML = "";
 
   //Row 5
-  document.getElementById("r5c1q").innerHTML = "17";
+  document.getElementById("r5c1q1").innerHTML = "17";
   document.getElementById("r5c1a1").innerHTML = "";
   document.getElementById("r5c1a2").innerHTML = "";
 
-  document.getElementById("r5c2q").innerHTML = "18";
+  document.getElementById("r5c2q1").innerHTML = "18";
   document.getElementById("r5c2a1").innerHTML = "";
   document.getElementById("r5c2a2").innerHTML = "";
 
-  document.getElementById("r5c3q").innerHTML = "19";
+  document.getElementById("r5c3q1").innerHTML = "19";
   document.getElementById("r5c3a1").innerHTML = "";
   document.getElementById("r5c3a2").innerHTML = "";
 
-  document.getElementById("r5c4q").innerHTML = "20";
+  document.getElementById("r5c4q1").innerHTML = "20";
   document.getElementById("r5c4a1").innerHTML = "";
   document.getElementById("r5c4a2").innerHTML = "";
-
-}
