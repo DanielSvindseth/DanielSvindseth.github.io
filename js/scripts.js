7765b3dc34f9f6f0c1cdb9dffe56bde8de0f4089
DanielSvindseth <daniel24.gitar@gmail.com>
2020-06-24 23:38:40 +0200

    Added new theme!

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,13 @@
 //much java such script
-function switchTheme(){
-  if (localStorage.theme==1) {
-    localStorage.setItem("theme", "2");
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm2.css');
-    return(0);
-  }
-  if (localStorage.theme==2) {
-    localStorage.setItem("theme", "3");
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm3.css');
-    return(0);
-  }
-  if (localStorage.theme==3) {
-    localStorage.setItem("theme", "4");
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm4.css');
-    return(0);
-  }
-  else {
-    localStorage.setItem("theme", "1");
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm1.css');
-  }
-}
 
 function setTheme(theme) {
   localStorage.setItem("theme", theme);
   document.getElementById('wow-theme').setAttribute('href', 'css/thm' + theme + '.css');
+}
+
+function loadTheme(){
+  var storedTheme = localStorage.getItem('theme');
+  document.getElementById('wow-theme').setAttribute('href', 'css/thm' + storedTheme + '.css');
 }
 
 function switchSet(){
@@ -52,24 +36,6 @@ function switchSet(){
   }
 }
 
-function loadTheme(){
-  if (localStorage.theme==1) {
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm1.css');
-  }
-  if (localStorage.theme==2) {
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm2.css');
-  }
-  if (localStorage.theme==3) {
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm3.css');
-  }
-  if (localStorage.theme==4) {
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm4.css');
-  }
-  else {
-    return(0);
-  }
-}
-
 function loadSet(){
   if (localStorage.flashcardset==1) {
     document.getElementById('currentSet').setAttribute('src', 'flashcards/set1.js');
@@ -94,10 +60,8 @@ function loadSet(){
 
 function firstVisit(){
   if (localStorage.prevVisit!=1) {
-    document.getElementById('wow-theme').setAttribute('href', 'css/thm1.css');
-    localStorage.setItem("theme", "1");
-
-
+    document.getElementById('wow-theme').setAttribute('href', 'css/thm4.css');
+    localStorage.setItem("theme", "4");
     localStorage.setItem("prevVisit", "1")
   }
 }
